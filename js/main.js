0765edd9f258627a8501a9407b2292577444db0b
Le Thanh Phuoc <thanhphuoc.se@gmail.com>
2022-03-13 13:19:58 +0700

    setup API and config

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+import postApi from './api/postApi';
+import studentApi from './api/studentApi';
 
+async function main() {
+  const queryParams = {
+    _page: 1,
+    _limit: 4,
+  };
+  await postApi.getAll(queryParams);
+}
+
+main();
