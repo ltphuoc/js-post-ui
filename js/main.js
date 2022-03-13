32799c2259cf1fe20ab26cad888831abb563026a
Le Thanh Phuoc <thanhphuoc.se@gmail.com>
2022-03-13 13:48:00 +0700

    config request response API

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@ import postApi from './api/postApi';
 import studentApi from './api/studentApi';
 
 async function main() {
-  const queryParams = {
-    _page: 1,
-    _limit: 4,
-  };
-  await postApi.getAll(queryParams);
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 4,
+    };
+    const data = await postApi.getAll(queryParams);
+    console.log(data);
+  } catch (error) {}
 }
 
 main();
